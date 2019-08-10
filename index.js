96e5eda05f36694738a40dbb437e5d01571d8de5
Nivetha <nivedha.orton@gmail.com>
2019-08-10 06:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
-function happyCountdown(holiday, days){
+function holidayCountdown(holiday, days){
   return "It\'s ${days} days untill ${holiday}!" ;
 }
